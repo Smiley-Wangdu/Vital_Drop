8828db5f82225d78a745f330f2ca9fec4c65c651
Bibika Shrestha <np03cs4a240020@heraldcollege.edu.np>
2026-05-10 23:30:17 +0545

    FIX: Enhance donor eligibility flow, fix registration page structure and added badge to the user profile.

--- auth/register_step2.php
+++ auth/register_step2.php
@@ -90,6 +90,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register - Vital Drop</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
 
 <?php include '../includes/navbar.php'; ?>
 
